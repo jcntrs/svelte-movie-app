5826a67abd809eee51a9af6b91da1cdb3f5c5a09
Jean C. Contreras <jean.cntrs@gmail.com>
2020-05-22 20:22:03 -0400

    Starting the project and creating the hello world

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let name;
+  let text = "This is Svelte";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <h5>{text}</h5>
+  <input type="text" bind:value={text} />
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
