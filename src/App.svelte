56253a29d332c3308d19c50e669082b87e2acd64
Jean C. Contreras <jean.cntrs@gmail.com>
2020-05-22 23:27:33 -0400

    Get api data

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,26 @@
 <script>
-  export let name;
-  let text = "This is Svelte";
+  import { onMount } from "svelte";
+  import Item from "./movie/Item.svelte";
+
+  const APIKEY = "00d23a7ba8835c665646fd7a63645408";
+  const BASEURL = "https://api.themoviedb.org/3";
+  const APISETTINGS = `?api_key=${APIKEY}&language=es-ES`;
+
+  let movies = [];
+
+  function getMovies() {
+    const URL = `${BASEURL}/discover/movie${APISETTINGS}&sort_by=popularity.desc`;
+    fetch(URL)
+      .then(response => response.json())
+      .then(({ results }) => {
+        movies = results;
+      });
+  }
+
+  onMount(() => {
+    console.log("the component has mounted");
+    getMovies();
+  });
 </script>
 
 <style>
@@ -11,12 +31,12 @@
     margin: 0 auto;
   }
 
-  h1 {
+  /*   h1 {
     color: #ff3e00;
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
-  }
+  } */
 
   @media (min-width: 640px) {
     main {
@@ -25,13 +45,23 @@
   }
 </style>
 
+<svelte:head>
+  <title>Svelte Movie App</title>
+  <link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous" />
+</svelte:head>
+
 <main>
-  <h1>Hello {name}!</h1>
-  <h5>{text}</h5>
-  <input type="text" bind:value={text} />
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
+  <div class="container">
+    <div class="row">
+      {#each movies as movie}
+        <div class="col-12 col-md-6 col-lg-3 p-2">
+          <Item />
+        </div>
+      {/each}
+    </div>
+  </div>
 </main>
---
+++ src/movie/Item.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<div class="card">
+  <img src="..." class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </p>
+    <div class="text-center">
+      <button class="btn btn-primary">Like</button>
+    </div>
+  </div>
+</div>
